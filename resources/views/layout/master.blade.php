1c2df78624a99a5034dd06f620dfcdb8f3912df2
Serafim <nesk@xakep.ru>
2015-09-24 06:14:03 +0300

    update resources

---
+++ resources/views/layout/master.blade.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="utf-8" />
+    <title>@section('title') GitterBot @show</title>
+</head>
+<body>
+    @yield('content')
+</body>
+</html>
