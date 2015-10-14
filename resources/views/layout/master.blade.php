44fc095671243248063487f586cc1e3d6c0e1038
Serafim <nesk@xakep.ru>
2015-10-15 00:09:29 +0300

    Update favicons

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -11,16 +11,16 @@
     <script src="{{ asset_ts('assets/app.js') }}"></script>
     <link rel="stylesheet" href="{{ asset_ts('assets/app.css') }}" />
 
-    <link rel="apple-touch-icon" href="/favicon.png" />
-    <link rel="icon" type="image/x-icon" href="/favicon.ico" />
-    <link rel="icon" type="image/png" href="/favicon.png" />
+    <link rel="apple-touch-icon"         href="{{ asset_ts('favicon.png') }}" />
+    <link rel="icon" type="image/x-icon" href="{{ asset_ts('favicon.ico') }}" />
+    <link rel="icon" type="image/png"    href="{{ asset_ts('favicon.png') }}" />
 </head>
 <body>
-    @yield('content')
-    <script>
-        (function(global) {
-            global['app'] = (new (require('Application'))).run();
-        })(window);
-    </script>
+@yield('content')
+<script>
+    (function(global) {
+        global['app'] = (new (require('Application'))).run();
+    })(window);
+</script>
 </body>
 </html>
