93f9fbb9a240f732bc0b6fcb54b421632bd81c15
Serafim <nesk@xakep.ru>
2015-10-16 08:56:53 +0300

    Improve codestyle

--- resources/views/page/user/aside.blade.php
+++ resources/views/page/user/aside.blade.php
@@ -1,27 +1,27 @@
 <aside class="profile-badge">
     <figure>
-        <img src="https://github.com/identicons/jasonlong.png" alt="User" data-bind="attr: {
-            src: avatar,
-            alt: name
-        }" />
+        <img src="https://github.com/identicons/jasonlong.png" alt="User"
+             data-bind="attr: { src: avatar, alt: name }" />
     </figure>
 
     <h1 data-bind="text: login">User Login</h1>
     <h2 data-bind="text: name">User Name</h2>
 
     <!--ko if: achievements.items().length-->
-    <section class="profile-achievements">
-        <!--ko foreach: achievements.items-->
-            <article class="achieve" data-bind="attr: {
-                title: title + '\nПолучено ' + created_at.toLocaleString()
-            }">
-                <img data-bind="attr: {src: image, alt: title}" />
-            </article>
-        <!--/ko-->
+        <section class="profile-achievements">
 
-        <a href="#" data-bind="click: $root.achievements">
-            к списку &raquo;
-        </a>
-    </section>
+            <!--ko foreach: achievements.items-->
+                <article class="achieve" data-bind="attr: {
+                    title: title + '\nПолучено ' + created_at.toLocaleString()
+                }">
+                    <img src="https://github.com/identicons/jasonlong.png"
+                         data-bind="attr: { src: image, alt: title }" />
+                </article>
+            <!--/ko-->
+
+            <a href="#" data-bind="click: $root.achievements">
+                к списку &raquo;
+            </a>
+        </section>
     <!--/ko-->
 </aside>
