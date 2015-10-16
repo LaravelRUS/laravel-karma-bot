b40b20736947c276dc8f2be34c73699efdccd4a2
Serafim <nesk@xakep.ru>
2015-10-16 05:43:46 +0300

    Improve noname google query

--- resources/lang/ru/google.php
+++ resources/lang/ru/google.php
@@ -1,5 +1,5 @@
 <?php
 return [
     'personal' => '@:user [погуглил для тебя](http://lmgtfy.com/?q=:query)',
-    'common'   => '[погуглил](http://lmgtfy.com/?q=:query)',
+    'common'   => '[помог погуглить](https://www.google.ru/webhp?hl=ru&q=:query)',
 ];
