111de413956db60a78f802aaebe5cf04dca9bee4
Serafim <nesk@xakep.ru>
2015-11-02 01:30:56 +0300

    Fix answers

--- app/Middlewares/PersonalAnswersMiddleware.php
+++ app/Middlewares/PersonalAnswersMiddleware.php
@@ -40,32 +40,35 @@ class PersonalAnswersMiddleware implements MiddlewareInterface
 
         if ($isBotMention) {
             $this->ai->handle($message);
-        }
 
 
+        } else {
 
-        // Hello all
-        $isHello = Str::contains($message->text_without_special_chars, [
-            'привет всем',
-            'всем привет'
-        ]);
+            // Hello all
+            $isHello = Str::contains($message->text_without_special_chars, [
+                'привет всем',
+                'всем привет'
+            ]);
 
-        if ($isHello) {
-            $id = array_rand(\Lang::get('personal.hello'));
+            if ($isHello) {
+                $id = array_rand(\Lang::get('personal.hello'));
 
-            $message->italic(\Lang::get('personal.hello.' . $id, [
-                'user' => $message->user->login
-            ]));
-        }
+                $message->italic(\Lang::get('personal.hello.' . $id, [
+                    'user' => $message->user->login
+                ]));
+            }
 
-        // Question
-        $isQuestion = Str::contains($message->text_without_special_chars, [
-            'можно задать вопрос',
-            'хочу задать вопрос'
-        ]);
 
-        if ($isQuestion) {
-            $message->italic(sprintf('@%s, и какой ответ ты ожидаешь услышать?', $message->user->login));
+            // Question
+            $isQuestion = Str::contains($message->text_without_special_chars, [
+                'можно задать вопрос',
+                'хочу задать вопрос'
+            ]);
+
+            if ($isQuestion) {
+                $message->italic(sprintf('@%s, и какой ответ ты ожидаешь услышать?', $message->user->login));
+            }
+            
         }
 
         return $message;
