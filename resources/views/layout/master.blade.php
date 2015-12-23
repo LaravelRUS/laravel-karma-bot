a265982a6848043150f4df6719d8cfdd7b05b11e
Serafim <nesk@xakep.ru>
2015-12-23 21:15:08 +0300

    Add ssl proto

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -8,12 +8,12 @@
         'csrf' => csrf_token(),
         'user' => Auth::user()
     ]) !!};</script>
-    <script src="{{ asset_ts('assets/app.js') }}"></script>
-    <link rel="stylesheet" href="{{ asset_ts('assets/app.css') }}" />
+    <script src="{{ asset_ts('assets/app.js', true) }}"></script>
+    <link rel="stylesheet" href="{{ asset_ts('assets/app.css', true) }}" />
 
-    <link rel="apple-touch-icon"         href="{{ asset_ts('favicon.png') }}" />
-    <link rel="icon" type="image/x-icon" href="{{ asset_ts('favicon.ico') }}" />
-    <link rel="icon" type="image/png"    href="{{ asset_ts('favicon.png') }}" />
+    <link rel="apple-touch-icon"         href="{{ asset_ts('favicon.png', true) }}" />
+    <link rel="icon" type="image/x-icon" href="{{ asset_ts('favicon.ico', true) }}" />
+    <link rel="icon" type="image/png"    href="{{ asset_ts('favicon.png', true) }}" />
 </head>
 <body>
 @yield('content')
