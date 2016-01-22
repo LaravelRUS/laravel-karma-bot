e6d2b4fff0e5022ee879ca8bd0e8f2013a61432f
Serafim <nesk@xakep.ru>
2016-01-22 15:54:20 +0300

    Add additional rooms

--- config/gitter.php
+++ config/gitter.php
@@ -8,11 +8,20 @@ return [
 
     'rooms'       => [
         //'debug' => env('GITTER_DEBUG_ROOM', '5617cdcad33f749381a8d5e5'), // Debug
-        'chat'  => '52f9b90e5e986b0712ef6b9d',  // Laravel Chat
-        'site'  => '54053e51163965c9bc201c26',  // Laravel Site
-        'bot'   => '560281040fc9f982beb1908a',  // Laravel Gitter Bot
 
-        'drupal' => '565c8d6716b6c7089cbcbd5d', // Temporary support for drupal.ru community
+        'laravel.chat'  => '52f9b90e5e986b0712ef6b9d',  // https://gitter.im/LaravelRUS/chat
+        'laravel.site'  => '54053e51163965c9bc201c26',  // https://gitter.im/LaravelRUS/laravel.ru
+        'laravel.bot'   => '560281040fc9f982beb1908a',  // https://gitter.im/LaravelRUS/GitterBot
+
+        'drupal'        => '565c8d6716b6c7089cbcbd5d', // https://gitter.im/DrupalRu/drupal.ru
+        'druio'         => '568a54e816b6c7089cc11010', // https://gitter.im/dru-io/Drupal
+
+        'yii.offtop'    => '55dc21c10fc9f982beae822c', // https://gitter.im/yiisoft/yii2/offtopic-rus
+        'yii.chat'      => '555086c915522ed4b3e03631', // https://gitter.im/yiisoft/yii2/rus
+
+        'phpua.symfony' => '5497ef29db8155e6700e1e81', // https://gitter.im/php-ua/symfony
+
+
     ],
 
 
