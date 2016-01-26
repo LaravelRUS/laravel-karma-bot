414c5263a8647d3a0bf762ce3d02e07f66545b78
atehnix <atehnix@gmail.com>
2016-01-26 15:57:40 +0300

    Update spelling and punctuation

--- resources/lang/ru/google.php
+++ resources/lang/ru/google.php
@@ -1,5 +1,5 @@
 <?php
 return [
-    'personal' => '@:user [погуглил для тебя](https://www.google.ru/webhp?#newwindow=1&hl=ru&q=:query)',
+    'personal' => '@:user, [погуглил для тебя](https://www.google.ru/webhp?#newwindow=1&hl=ru&q=:query)',
     'common'   => '[помог погуглить](https://www.google.ru/webhp?#newwindow=1&hl=ru&q=:query)',
 ];
