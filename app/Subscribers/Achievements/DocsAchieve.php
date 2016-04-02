e0108b8f20e1b67ef232f68d56040fa95f0112a7
Serafim <nesk@xakep.ru>
2016-04-02 19:29:03 +0300

    Add docs achieve

---
+++ app/Subscribers/Achievements/DocsAchieve.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * This file is part of GitterBot package.
+ *
+ * @author Serafim <nesk@xakep.ru>
+ * @date 11.10.2015 6:07
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace App\Subscribers\Achievements;
+
+use App\Karma;
+use App\Gitter\Achieve\AbstractAchieve;
+
+/**
+ * Class DocsAchieve
+ * @package App\Achieve
+ */
+class DocsAchieve extends AbstractAchieve
+{
+    /**
+     * @var string
+     */
+    public $title = 'Красава';
+
+    /**
+     * @var string
+     */
+    public $description = 'Помог переводить документацию. Настоящий мужик!';
+
+    /**
+     * @var string
+     */
+    public $image = '//karma.laravel.su/img/achievements/docs.gif';
+
+    /**
+     * @throws \LogicException
+     */
+    public function handle()
+    {
+        // Only manual addition
+    }
+}
