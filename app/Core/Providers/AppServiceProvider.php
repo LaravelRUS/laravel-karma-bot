56c45b66356046269b5301d424c871e292d10449
Serafim <nesk@xakep.ru>
2016-04-12 22:48:32 +0300

    Add date registrations

--- app/Core/Providers/AppServiceProvider.php
+++ app/Core/Providers/AppServiceProvider.php
@@ -2,27 +2,23 @@
 
 namespace Core\Providers;
 
+use Carbon\Carbon;
+use Illuminate\Container\Container;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
-    }
-
-    /**
-     * Register any application services.
-     *
      * @return void
      */
     public function register()
     {
-        //
+        $this->app->singleton(\DateTimeZone::class, function() {
+            return Carbon::now()->getTimezone();
+        });
+
+        $this->app->bind(\DateTime::class, function(Container $app) {
+            return new \DateTime('now', $app->make(\DateTimeZone::class));
+        });
     }
 }
