baf846dd034aa2e33cc655638c55c760228316e6
Kirill Nesmeyanov <nesk@xakep.ru>
2016-07-15 15:04:44 +0300

    Update gitter.php
    Remove n2o and add SleepingOwlAdmin chat

--- config/gitter.php
+++ config/gitter.php
@@ -12,6 +12,7 @@ return [
         'laravel.chat'  => '52f9b90e5e986b0712ef6b9d',  // https://gitter.im/LaravelRUS/chat
         'laravel.site'  => '54053e51163965c9bc201c26',  // https://gitter.im/LaravelRUS/laravel.ru
         'laravel.bot'   => '560281040fc9f982beb1908a',  // https://gitter.im/LaravelRUS/GitterBot
+        'laravel.so'    => '56cb1cf8e610378809c2ca4d',  // https://gitter.im/LaravelRUS/SleepingOwlAdmin
 
         'drupal'        => '565c8d6716b6c7089cbcbd5d', // https://gitter.im/DrupalRu/drupal.ru
         'druio'         => '568a54e816b6c7089cc11010', // https://gitter.im/dru-io/Drupal
@@ -23,8 +24,6 @@ return [
         'phpua.php'     => '549565c0db8155e6700e16c1', // https://gitter.im/php-ua/php
 
         'vuejs.chat'    => '55d5eaab0fc9f982beae0db8', // https://gitter.im/vuejs-ru/Discussion
-        
-        'synrc.n2o'     => '54a2823adb8155e6700e3f37', // https://gitter.im/synrc/n2o
     ],
 
 
