9ea3f844ad5ab4b9939241599e925d37856ad846
Андрей Соснов <atehnix@gmail.com>
2016-08-02 03:25:56 +0300

    Update long.php
    Very small fix. )

--- resources/lang/ru/long.php
+++ resources/lang/ru/long.php
@@ -4,7 +4,7 @@ return [
     'text_personal' =>
         "[user]:user[/user], у нас тут принято писать кратко и по делу. А если нужно показать длинный код, то можно ".
         "использовать pastebin-сервис, например [url=https://gist.github.com/]gist.github.com[/url]".
-        "или [url=http://laravel.io/bin]laravel.io/bin[/url]."
+        " или [url=http://laravel.io/bin]laravel.io/bin[/url]."
     ,
     'code_personal' =>
         "[user]:user[/user], чтобы показать длинный листинг кода желательно использовать pastebin-сервис, например " .
