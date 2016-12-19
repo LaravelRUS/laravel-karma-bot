775d52f1e60e77c809bf5293a7bd3df6d49ebdbe
Nesmeyanov Kirill <k.nesmeyanov@rambler-co.ru>
2016-12-19 21:33:26 +0300

    Improve docker

--- docker/bot-php/start.sh
+++ docker/bot-php/start.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
+php -r "copy('https://getcomposer.org/composer.phar', 'composer.phar');"
+php composer.phar install
+rm composer.phar
 
 # KarmaBot
 cd /var/www/karmabot
 cp -n ../.env.example .env
-
-
-php -r "copy('https://getcomposer.org/composer.phar', 'composer.phar');"
-php composer.phar install
-rm composer.phar
 
 php artisan key:generate
 php artisan migrate --force --seed
