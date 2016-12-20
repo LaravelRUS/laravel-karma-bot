3708ec67e37c3c6219aaaeef5eeff857d54da55e
Serafim <nesk@xakep.ru>
2016-12-20 06:48:01 +0300

    It works! Windows, Karl!

--- docker/php-bot/start.sh
+++ docker/php-bot/start.sh
@@ -2,14 +2,12 @@
 
 cd /var/www
 
-php -r "copy('https://getcomposer.org/composer.phar', 'composer.phar');"
-php composer.phar install
-rm composer.phar
-rm -rf .composer
+php -v
 
-# KarmaBot
+php composer.phar install
+cp -n .env.example ./karmabot/.env
+
 cd /var/www/karmabot
-cp -n ../.env.example .env
 
 php artisan key:generate
 php artisan migrate --force --seed
