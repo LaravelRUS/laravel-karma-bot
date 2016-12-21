fec9b9f6bcd2a652c64565e654c9a426aa0095ab
Nesmeyanov Kirill <k.nesmeyanov@rambler-co.ru>
2016-12-21 18:43:19 +0300

    Remove unused items from docker

--- docker/php-bot/start.sh
+++ docker/php-bot/start.sh
@@ -2,12 +2,21 @@
 
 cd /var/www
 
+chown www-data:www-data ./karmabot -R
+chmod 0777 ./karmabot
+
 php -v
 
-php composer.phar install
-cp -n .env.example ./karmabot/.env
+rm /var/www/karmabot/storage/logs/laravel.log
+touch /var/www/karmabot/storage/logs/laravel.log
+tail -f /var/www/karmabot/storage/logs/laravel.log
+
 
 cd /var/www/karmabot
+
+cp -n ../.env.example .env
+
+composer install
 
 php artisan key:generate
 php artisan migrate --force --seed
